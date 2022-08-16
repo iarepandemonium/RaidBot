a54fe4598c21c2c4479c95bacfab4fbe3203e107
Pandette <iarepandette@gmail.com>
2022-08-15 21:03:52 -0400

    config not saving?

--- src/main/java/net/pandette/configuration/PingData.java
+++ src/main/java/net/pandette/configuration/PingData.java
@@ -1,11 +1,15 @@
 package net.pandette.configuration;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class PingData {
+    @SerializedName("pingRole")
     private final String pingRole;
+
+    @SerializedName("words")
     private final List<String> words;
 }
